88c209b0e52b7d63992ecf430c3190a1d4f2e765
GustavoQueiros <gustavo.dev47@gmail.com>
2024-11-12 21:32:07 -0300

    criação das tabelas manualmente pelos arquivos .sql

--- locadora/src/main/java/edu/adatech/locadoradeveiculos/model/ClienteModel.java
+++ locadora/src/main/java/edu/adatech/locadoradeveiculos/model/ClienteModel.java
@@ -1,24 +1,28 @@
 package edu.adatech.locadoradeveiculos.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "CLIENTES")
 public class ClienteModel {
 
     @Id
+    @Getter
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Getter
     private String nome;
 
+    @Getter
     @Setter
     private String logradouro;
-
-    public String getNome() {
-        return nome;
-    }
 
     public void setNome(String nome) {
         if (nome != null && nome.matches("[a-zA-Z\\s]+")) {
@@ -28,11 +32,4 @@ public class ClienteModel {
         }
     }
 
-    public String getLogradouro() {
-        return logradouro;
-    }
-
-    public void setLogradouro(String logradouro) {
-        this.logradouro = logradouro;
-    }
 }
